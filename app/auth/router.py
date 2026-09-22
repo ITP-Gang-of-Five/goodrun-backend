0ea44e4540b03dc86459f1c6da964ced962563b3
maximus-debski <122778920+maximus-debski@users.noreply.github.com>
2026-09-23 07:29:56 +1000

    Updated login code to work with new fake json databse schema

--- app/auth/router.py
+++ app/auth/router.py
@@ -10,7 +10,7 @@ from app.auth.schemas import (
 )
 from app.auth.tokens import make_token, read_token
 from app.errors import ApiError
-from app.storage import get_users
+from app.storage import get_database
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -19,7 +19,7 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 def login(body: LoginRequest) -> LoginResponse:
 
     # grab the user by their email (since that is what is inputted into the request body)
-    user = get_users().get_by_email(body.email)
+    user = get_database().get_user_by_email(body.email)
 
     # if user doesn't exit or the password isn't correct (NB: will use actual encryption and things later)
     if user is None or user.password != body.password:
@@ -39,7 +39,7 @@ def refresh_token(body: RefreshRequest) -> TokenPair:
     # grab the user by their refresh token
     # NB: if the token is invalid for the user, an exception will be raised here and the request
     # will get an error code returend for it
-    user = get_users().get_by_id(read_token(body.refresh_token, "refresh"))
+    user = get_database().get_user(read_token(body.refresh_token, "refresh"))
     # if no user is assigned to this refresh token
     if user is None:
         raise ApiError(401, "UNAUTHORISED", "User doesn't exist")
